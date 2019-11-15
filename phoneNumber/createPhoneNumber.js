dda7a7ef85b743929728cb0cbf3678a7c8516a91
Janet <44039641+jaurinu@users.noreply.github.com>
2019-11-14 20:05:04 -0600

    Update createPhoneNumber.js

--- phoneNumber/createPhoneNumber.js
+++ phoneNumber/createPhoneNumber.js
@@ -1,11 +1,7 @@
 const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
 const phoneNumber = (number) => {
-let firstArray = number.slice(0,3).join('');
-let secondtArray = number.slice(3,6).join('');
-let thirdArray = number.slice(6,9).join('');
-
-let newphoneNumber = "(" + firstArray + ") " + secondtArray + "-" + thirdArray;
-return newphoneNumber;
+let newPhone = "(" + [number.slice(0,3).join('')] + ") " + [number.slice(3,6).join('')] + "-" + [number.slice(6,9).join('')]
+return newPhone;
 };
-console.log(phoneNumber(number));
+phoneNumber(number);
